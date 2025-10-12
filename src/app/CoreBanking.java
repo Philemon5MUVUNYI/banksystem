1784d8a83902a782e448d361869f168cbdcbe7ae
Philemon5MUVUNYI <muvunyiphilemon22@gmail.com>
2025-10-12 23:22:47 +0200

    commit message

--- src/app/CoreBanking.java
+++ src/app/CoreBanking.java
@@ -103,7 +103,7 @@ public class CoreBanking {
                         if(out_display.equalsIgnoreCase("Y")){
                             condition = true;
                         }else {
-                            System.out.println("====The End====dfdv=");
+                            System.out.println("====The End====");
                             condition = false;
                         }
 
